b4b22ee6f28c9278a9e86227714ba1a1420bf1b3
MichalNowa <michall.nowakowskiii@gmail.com>
2022-04-03 20:54:36 +0200

    v0.2

--- WinForm Contest/Contest/Form1.Designer.cs
+++ WinForm Contest/Contest/Form1.Designer.cs
@@ -28,45 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.logbutton = new System.Windows.Forms.Button();
-            this.logtxt = new System.Windows.Forms.TextBox();
-            this.passtxt = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxlogin = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxpass = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
-            // logbutton
+            // label1
             // 
-            this.logbutton.Location = new System.Drawing.Point(265, 326);
-            this.logbutton.Name = "logbutton";
-            this.logbutton.Size = new System.Drawing.Size(241, 90);
-            this.logbutton.TabIndex = 1;
-            this.logbutton.Text = "button2";
-            this.logbutton.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(185, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login";
             // 
-            // logtxt
+            // textBoxlogin
             // 
-            this.logtxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.logtxt.Location = new System.Drawing.Point(265, 86);
-            this.logtxt.Name = "logtxt";
-            this.logtxt.Size = new System.Drawing.Size(241, 31);
-            this.logtxt.TabIndex = 2;
+            this.textBoxlogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxlogin.Location = new System.Drawing.Point(12, 83);
+            this.textBoxlogin.Name = "textBoxlogin";
+            this.textBoxlogin.Size = new System.Drawing.Size(253, 38);
+            this.textBoxlogin.TabIndex = 1;
             // 
-            // passtxt
+            // label2
             // 
-            this.passtxt.BackColor = System.Drawing.SystemColors.HotTrack;
-            this.passtxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.passtxt.Location = new System.Drawing.Point(265, 198);
-            this.passtxt.Name = "passtxt";
-            this.passtxt.Size = new System.Drawing.Size(241, 31);
-            this.passtxt.TabIndex = 3;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(131, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 31);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Password";
+            // 
+            // textBoxpass
+            // 
+            this.textBoxpass.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxpass.Location = new System.Drawing.Point(12, 158);
+            this.textBoxpass.Name = "textBoxpass";
+            this.textBoxpass.PasswordChar = '*';
+            this.textBoxpass.Size = new System.Drawing.Size(253, 38);
+            this.textBoxpass.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(12, 250);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(253, 66);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Log in";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 364);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "To register click ";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(89, 364);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(37, 13);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "HERE";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // Contest
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 461);
-            this.Controls.Add(this.passtxt);
-            this.Controls.Add(this.logtxt);
-            this.Controls.Add(this.logbutton);
+            this.ClientSize = new System.Drawing.Size(277, 386);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxpass);
+            this.Controls.Add(this.textBoxlogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.MaximumSize = new System.Drawing.Size(800, 500);
             this.Name = "Contest";
             this.Text = "Form1";
@@ -77,9 +127,14 @@
         }
 
         #endregion
-        private System.Windows.Forms.Button logbutton;
-        private System.Windows.Forms.TextBox logtxt;
-        private System.Windows.Forms.TextBox passtxt;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxlogin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxpass;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
 
--- WinForm Contest/Contest/Form1.cs
+++ WinForm Contest/Contest/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Diagnostics;
 
 namespace Contest
 {
@@ -16,15 +18,17 @@ namespace Contest
         {
             InitializeComponent();
             this.MaximizeBox = false;
-            this.BackColor = Color.FromArgb(50, 170, 206);
-            logbutton.Location = new Point(this.Width /2 - logbutton.Width/2, this.Height/2 + logbutton.Height);
-            logtxt.Location = new Point(this.Width/2 - logtxt.Width/2, this.Height/5 + logtxt.Height);
-            passtxt.Location = new Point(this.Width / 2 - passtxt.Width / 2, this.Height / 2 - passtxt.Height);
+
         }
 
         private void Contest_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley");
         }
     }
 }
